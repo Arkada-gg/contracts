2dd8e1463e83d2da44280bc64d241771f53af6b8
0xtdlArk <ilyat@arkada.gg>
2025-03-14 12:53:00 +0100

    feat: erc721 with trade func

---
+++ contracts/interfaces/IArkadaRewarderWithTrade.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+/**
+ * @title IArkadaRewarderWithTrade
+ * @author Arkada
+ */
+interface IArkadaRewarderWithTrade is IArkadaRewarder {
+    /// @notice Event emitted when trade ERC721 is updated
+    /// @param oldTradeERC721 Address of the old trade ERC721
+    /// @param newTradeERC721 Address of the new trade ERC721
+    event TradeERC721Updated(
+        address indexed oldTradeERC721,
+        address indexed newTradeERC721
+    );
+
+    /**
+     * @notice Allows a user to trade their old NFT for a new one by burning the old NFT.
+     * @param tokenId The token ID of the BUSHI NFT to be burned.
+     */
+    function tradeNft(uint256 tokenId) external;
+}
